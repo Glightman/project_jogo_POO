5cee2837a6f30e135d500cbde463839979ae773a
AndressaMiyaguti <83734814+AndressaMiyaguti@users.noreply.github.com>
2021-05-27 22:26:11 -0300

    building code

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+from relogio import Relogio
+relogio = Relogio()
 class Steve:
     def __init__(self):
         self.energia = 10
@@ -11,6 +13,10 @@ class Steve:
     
     def decor(self):
         print('-=' * 20)
+    
+    def ler(self):
+        relogio.avancaTempo(35)
+        self.inteligência += 2
     
     def dados(self):
         print(f'''
@@ -28,54 +34,78 @@ class Steve:
 
     #def alimento(self ):
      #  self.comida -= 2
-   
+
+    def morrer(self):
+        if self.fome == 10 or self.sede == 10 or self.energia == 0 or self.saúde == 0:
+            print('FIM DO JOGO, VOCÊ MORREU :X ')
+           
       
     
     
     def Comer( self):
+        relogio.avancaTempo(35)
         self.fome -= 2
         self.saúde += 0.5
-        self.comida -= 2     
-        print(self.dados)
+        self.comida -= 2   
+        if self.fome == 9:
+            print('SE VOCÊ NÃO SE ALIMENTAR IRÁ MORRER')  
+      
     
     def Beber (self):
-        self.sede -= 2
-        self.saúde += 0.5
+        relogio.avancaTempo(5)
+        if self.sede > 1 :
+            self.sede -= 2
+            self.saúde += 0.5
+        elif self.sede == 1 :
+            self.sede -= 1
+        else:
+            print('VOCÊ NÃO ESTÁ COM SEDE.')
+        
        
     def Estudar(self):
+        relogio.avancaTempo(240)
         self.energia -= 4
         self.fome += 2
         self.sede += 2
         self.inteligência += 3
     def Malhar (self):
+        relogio.avancaTempo(60)
         self.energia -= 3
         self.fome += 2
         self.sede += 2 
         self.saúde += 2
     def Comprar(self):
+        relogio.avancaTempo(60)
         self.energia -= 0.5
         self.dinheiro -= 100
         self.comida += 2
     def Trabalhar(self):
+        relogio.avancaTempo(540)
         self.energia -= 2
         self.fome += 1
         self.sede += 1
         self.saúde -= 1
         self.dinheiro += 120
     def sono(self):
+        relogio.avancaTempo(30)
         self.energia += 5
     
     def Medicacao(self):
+        relogio.avancaTempo(2)
         self.saúde += 1
         self.remedio -= 1
     
     def Dormir(self):
+        relogio.avancaTempo(540)
         self.saúde += 2
         self.energia = 0
         self.energia = 20
+        if self.fome == 5:
+            self.fome += 5
     
     def opcao(self):
-        while True:
+        while not self.fome == 10 or self.sede == 10 or self.energia == 0 or self.saúde == 0:  
+
             opcao = int(input('''ESTES SÃO SEUS AMBIENTES:
             [ 1 ]  CASA
             [ 2 ]  BAR
@@ -87,9 +117,11 @@ class Steve:
             [ 0 ]  SAIR
             --> : '''))
             if opcao == 1 :
-                self.casa
+                self.casa()
+        self.morrer()            
     def casa(self):
-        while True:
+        while not self.fome == 10 or self.sede == 10 or self.energia == 0 or self.saúde == 0:
+            self.morrer()
             casas = int(input('''ESSAS SÃO SUAS OPÇÕES NA CASA:
             [ 1 ] COMER
             [ 2 ] BEBER ÁGUA
@@ -110,7 +142,7 @@ class Steve:
                 self.Medicacao()
                
             elif casas == 4:
-                self.Estudar()
+                self.ler()
                 
             elif casas == 5:
                 self.sono()
@@ -118,12 +150,13 @@ class Steve:
             elif casas == 6:
                 self.Dormir()
             elif casas == 7:
-                self.dados
+                self.dados()
             else:
                 self.decor()
                 print('VOCÊ SAIU DO JOGO')
                 self.decor()
                 break
+        self.morrer()
 
 
             
