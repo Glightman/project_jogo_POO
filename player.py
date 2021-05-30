0747299844ef71d76ede8d31255857f673403904
Gabriel Santana <gabriellima36716@gmail.com>
2021-05-30 16:09:19 -0300

    redoing all

--- player.py
+++ player.py
@@ -2,7 +2,6 @@ from relogio import Relogio
 from time import sleep
 relogio = Relogio()
 dia = Relogio()
-class Steve:
 class Steve: #AQUI NÓS CRIAMOS UMA CLASSE PARA DEFINIR OS ATRIBUTOS DO NOSSO PERSONAGEM
     def __init__(self):
         self.energia = 10 #NO ATRIBUTO ENERGIA ELE JA INICIA COM O MAX POIS ESSE É UM DOS ATRIBUTOS QUE VAI MANDAR NA VIDA DO PERSONAGEM
@@ -44,11 +43,6 @@ class Steve: #AQUI NÓS CRIAMOS UMA CLASSE PARA DEFINIR OS ATRIBUTOS DO NOSSO PE
     # E TODAS ELAS SEGUEM O MESMO MODELO:... 
     # AO SER ACIONADAS ELAS ALTERAM OS ATRIBUTOS OU PRINTAM ALGO NA TELA.
 
-    def beberVinho(self):
-        relogio.avancaTempo(40)
-        self.sede += 1
-        self.saúde += 1    
-    
     def comer(self):
         relogio.avancaTempo(35)
         self.fome -= 2
@@ -80,20 +74,6 @@ class Steve: #AQUI NÓS CRIAMOS UMA CLASSE PARA DEFINIR OS ATRIBUTOS DO NOSSO PE
             self.sede -= 1
         else:
             print('\033[33mVOCÊ NÃO ESTÁ COM SEDE.\033[04;37m')
-        
-    def estudar(self):
-        relogio.avancaTempo(240)
-        self.energia -= 4
-        self.fome += 2
-        self.sede += 2
-        self.inteligência += 3
-    
-    def malhar(self):
-        relogio.avancaTempo(60)
-        self.energia -= 3
-        self.fome += 2
-        self.sede += 2 
-        self.saúde += 2
     
     def Comprar(self):
         relogio.avancaTempo(60)
@@ -201,52 +181,7 @@ class Steve: #AQUI NÓS CRIAMOS UMA CLASSE PARA DEFINIR OS ATRIBUTOS DO NOSSO PE
                 self.decor()
                 break
         self.morrer()
-
-    def bar(self):
-        vinho = 0
-        
-        while True:
-            per = int(input('''SEJA BEM VINDO
-            TEMOS APENAS VINHO EM NOSSO CARDÁPIO.
-            QUER VER AS OPÇÕES?
-            [ 1 ] SIM
-            [ 2 ] MOSTRAR SEUS ATRIBUTOS
-            [ 3 ] NÃO/SAIR
-            '''))
-            if per == 1:
-                while True:
-                    vin = int(input('''
-                     [ 1 ] VINHO ARGENTINO ( DOSE )......R$  15,00
-                     [ 2 ] VINHO CHILENO  ( DOSE ).......R$  15,00
-                     [ 3 ] FINALIZAR PEDIDO
-                     [ 0 ] VOLTAR
-                '''))
-                    soma = vinho * 15.0
-                    if vin == 1 or vin == 2:
-                        vinho += 1
-                    elif vin == 3:
-                        print(f'O VALOR TOTAL DO SEU PEDIDO É DE R$ {soma:.2f}')
-                        per1 =int(input('''
-                        POSSO FINALIZAR O PEDIDO?
-                        [ 1 ] SIM
-                        [ 2 ] NÃO
-                        '''))
-                        if per1 == 1 and self.dinheiro < soma :
-                            print(f'''
-                                    INFELIZMENTE O VALOR DE 
-                                    R$ {soma:.2f} FOI RECUSADO,
-                                    PELA OPERADORA DO CARTÃO.
-                                    VERIFIQUE SUA CONTA''')
-                            break      
-                        else:
-                            relogio.avancaTempo(30)
-                            self.beberVinho()
-                            print('SEU PEDIDO SERÁ SERVIDO NA MESA 12')
-            elif per == 2:
-                self.dados()
-            else:
-                break
-  
+    
     def trabalho(self):
         import time
 
@@ -307,105 +242,3 @@ class Steve: #AQUI NÓS CRIAMOS UMA CLASSE PARA DEFINIR OS ATRIBUTOS DO NOSSO PE
                     
             else:
                 print('OPÇÃO INVÁLIDA')
-
-        
-    
-    def escola(self):
-        parar = 0
-        while parar == 0:
-            opt = int(input('''SEJA BEM VINDO AO PY'SCHOOL
-            [ 1 ] Estudar
-            [ 2 ] Beber água
-            [ 3 ] Voltar
-            --> : '''))
-            if opt == 1:
-                self.estudar
-            elif opt == 2:
-                self.beber
-            elif opt == 3:
-                parar += 1
-            else:
-                print('Esta opção não é válida, tente outra: ')
-
-    def academia(self):
-            parar = 0
-            while parar == 0:
-                opt = int(input('''SEJA BEM VINDO A ACADEMIA PY'FIT
-                    [ 1 ] Malhar
-                    [ 2 ] Beber água
-                    [ 3 ] Mostrar atributos
-                    [ 4 ] Voltar
-                    --> : '''))
-                if opt == 1:
-                    self.malhar
-                elif opt == 2:
-                    self.beber
-                elif opt == 3:
-                    self.dados
-                elif opt == 4:
-                    parar += 1
-                else:
-                    print('Esta opção não é válida, tente outra: ')
-    def restaurante(self):
-        relogio.avancaTempo(60)
-        bife = 0
-        frango = 0
-        suco = 0
-        agua = 0
-        print('SEJA BEM VINDO!!')
-        while True:
-            opcao = int(input(''' 
-        EM NOSSO CARDAPIO TEMOS AS SEGUINTES OPÇÕES:
-        [ 1 ] PF BIFE COM BATATA FRITA....... R$ 18,00
-        [ 2 ] PF FRANGO A PASSARINHO......... R$ 15,00
-        [ 3 ] SUCO DE LARANJA  500ML......... R$  8,00
-        [ 4 ] ÁGUA MINERAL S/ GAS  510ML......R$  4,00
-        [ 5 ] CONFIRMAR PEDIDO
-        [ 0 ] SAIR DO RESTAURANTE
-        FAÇA SEU PEDIDO
-        '''))
-            soma = (bife * 18.0) + ( frango * 15.0) + (suco * 8.0) + (agua * 4.0)
-            if opcao == 1 :
-                bife += 1
-            elif opcao == 2 :
-                frango += 1
-            elif opcao == 3:
-                suco += 1
-            elif opcao == 4:
-                agua += 1
-            elif opcao == 5:
-                while True:
-                    per = int(input(f'''VOCÊ CONFIRMA O PEDIDO?
-
-                    [ {bife} ] PF BIFE COM BATATA FRITA
-                    [ {frango} ] PF FRANGO A PASSARINHO
-                    [ {suco} ] SUCO DE LARANJA  500ML
-                    [ {agua} ] ÁGUA  500ML     
-
-            
-                    [ 1 ] SIM
-                    [ 0 ] NÃO
-                    '''))
-                    if per == 1:
-                        print(f'''
-                        SEU PEDIDO SERÁ PREPARADO, 
-                        APÓS A CONFIRMAÇÃO DE PAGAMENTO
-                        O VALOR TOTAL É DE R$ {soma:.2f}
-                        ''')
-                    if per == 1 and self.dinheiro < soma :
-                        print(f'''
-                        VOCÊ POSSUI  R${self.dinheiro:.2f} EM 
-                        SUA CONTA, E NÃO É SUFICIENTE PARA FINALIZAR
-                        O PEDIDO.
-                        ''')           
-                        break         
-                    else:
-                        bife = 0 
-                        frango = 0
-                        suco = 0 
-                        agua = 0 
-            elif opcao == 0 :
-                print('VOLTE SEMPRE')
-                break                   
-            else:
-                print('NÚMERO INVÁLIDO. TENTE NOVAMENTE')
